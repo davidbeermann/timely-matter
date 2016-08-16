30b4f3415ed6a8ac6a68f449189f40c8f4779047
David Beermann <mail@davidbeermann.com>
2016-08-16 16:08:02 +0200

    added missing comment for resource

--- application/timely-matter/src/VectorFieldInputFactory.h
+++ application/timely-matter/src/VectorFieldInputFactory.h
@@ -17,5 +17,8 @@ enum class VectorFieldInputType : uint8_t {
 
 class VectorFieldInputFactory {
 public:
+    // You cannot create an object of an abstract class type;
+    // however, you can use pointers and references to abstract class types.
+    // https://msdn.microsoft.com/en-us/library/c8whxhf1.aspx
     VectorFieldInputProvider* get(VectorFieldInputType type);
 };
