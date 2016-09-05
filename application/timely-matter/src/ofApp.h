8fbb3f063e012b7a3cb1789e7877ba7b889e167f
David Beermann <mail@davidbeermann.com>
2016-09-05 19:20:56 +0200

    removed old code

--- application/timely-matter/src/ofApp.h
+++ application/timely-matter/src/ofApp.h
@@ -12,13 +12,6 @@
 #include "UpdateDepthCalibrationCommand.hpp"
 #include "ViewManager.hpp"
 #include "GuiView.hpp"
-//#include "ofEvents.h"
-//#include "AppUI.hpp"
-//#include "AppVars.hpp"
-//#include "VectorFieldInputFactory.h"
-//#include "VectorFieldInputProvider.h"
-//#include "VectorField.h"
-//#include "ParticleSystem.h"
 
 using namespace timelymatter;
 
@@ -46,26 +39,3 @@ public:
     void draw();
 };
 
-
-//class ofApp : public ofBaseApp {
-//    void m_prepare_for_rendering();
-//    
-//    AppUI m_ui;
-//    VectorFieldInputFactory m_input_factory;
-//    VectorFieldInputProvider* m_input_provider;
-//    VectorField m_vector_field;
-//    ParticleSystem m_particle_system;
-//    bool m_is_prepared;
-//    
-//    ofVec2f mCenteredPos;
-//public:
-//    void setup();
-//    void update();
-//    void draw();
-//    void windowResized(int w, int h);
-//    
-//    void updateGuiParameters();
-//    
-//    const bool isInputAvailable();
-//    void setInputProvider(AppMode& mode);
-//};
