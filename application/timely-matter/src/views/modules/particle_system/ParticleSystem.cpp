387dbf4d660cf84c9327ab52c92646f7e0f32444
David Beermann <mail@davidbeermann.com>
2016-11-28 20:27:00 +0100

    added some visual features for first demo

--- application/timely-matter/src/views/modules/particle_system/ParticleSystem.cpp
+++ application/timely-matter/src/views/modules/particle_system/ParticleSystem.cpp
@@ -9,12 +9,35 @@ void ParticleSystem::setup(const unsigned int num_particles, const ofVec3f size)
     // define bounding box for particles
     m_bounds.set(0, 0, size.x, size.y);
     
+    // allocate buffers
+    m_particles_fbo.allocate(size.x, size.y);
+    m_lines_fbo.allocate(size.x, size.y);
+    m_output_fbo.allocate(size.x, size.y);
+    
+    // clear buffers
+    m_particles_fbo.begin();
+    ofClear(0);
+    m_particles_fbo.end();
+    
+    m_lines_fbo.begin();
+    ofClear(0);
+    m_lines_fbo.end();
+    
+    m_output_fbo.begin();
+    ofClear(0);
+    m_output_fbo.end();
+    
+    // setup lines mesh
+    m_lines_mesh.setMode(OF_PRIMITIVE_LINES);
+    
     // setup GUI parameters
     m_params.setName("Particle System");
     m_params.add(m_show_particles.set("show particles", true));
     m_params.add(m_max_velocity.set("max velocity", 5.f, 1.f, 10.f));
     m_params.add(m_velocity_decay.set("velocity decay", 0.99f, 0.9f, 0.999f));
-    m_params.add(m_show_mark_reference.set("show mark ref", true));
+    m_params.add(m_show_mark_reference.set("show mark ref", false));
+    m_params.add(m_draw_lines.set("draw lines", false));
+    m_params.add(m_draw_fading.set("draw fading", false));
     
     // add particles to system.
     // this has to happen after the GUI setup, due to the max velocity parameter.
@@ -28,7 +51,7 @@ void ParticleSystem::setup(const unsigned int num_particles, const ofVec3f size)
     m_particle_mesh.setMode(OF_PRIMITIVE_TRIANGLE_FAN);
     m_particle_mesh.addVertex(ofVec3f(0.f, 0.f, 0.f));
     int steps = 12;
-    float radius = 3.f;
+    float radius = 1.5f;
     double inc = TWO_PI / (double) steps;
     for (int i = 0; i <= steps; ++i) {
         double radian = i * inc;
@@ -58,7 +81,28 @@ void ParticleSystem::update() {
 
 void ParticleSystem::draw(const VectorField& vectorField) {
     if (m_show_particles) {
+        
+        m_lines_mesh.clear();
+        
+        m_particles_fbo.begin();
+        ofClear(0);
+        
         for (PIt p = m_particles.begin(); p != m_particles.end(); ++p) {
+            
+            if (m_draw_lines) {
+                for (PIt p2 = p + 1; p2 != m_particles.end(); ++p2) {
+                    float dist = abs(p->getPosition().distance(p2->getPosition()));
+                    if (dist <= 50) {
+                        
+                        m_lines_mesh.addVertex(p->getPosition());
+                        m_lines_mesh.addVertex(p2->getPosition());
+                        m_lines_mesh.addColor(ofColor(255));
+                        m_lines_mesh.addColor(ofColor(255));
+    //                    m_lines_mesh.addColor(ofColor(ofRandom(255), ofRandom(255), ofRandom(255)));
+    //                    m_lines_mesh.addColor(ofColor(ofRandom(255), ofRandom(255), ofRandom(255)));
+                    }
+                }
+            }
             
             p->draw(m_particle_mesh);
             
@@ -70,5 +114,31 @@ void ParticleSystem::draw(const VectorField& vectorField) {
                 ofPopStyle();
             }
         }
+        
+        m_particles_fbo.end();
+        
+        if (m_draw_lines) {
+            m_lines_fbo.begin();
+            ofClear(0);
+            m_lines_mesh.draw();
+            m_lines_fbo.end();
+        }
+        
+        m_output_fbo.begin();
+        if (m_draw_fading) {
+            ofPushStyle();
+            ofSetColor(33, 33, 33, 9);
+            ofDrawRectangle(0, 0, m_output_fbo.getWidth(), m_output_fbo.getHeight());
+            ofPopStyle();
+        } else {
+            ofClear(0);
+        }
+        if (m_draw_lines) {
+            m_lines_fbo.draw(0, 0);
+        }
+        m_particles_fbo.draw(0, 0);
+        m_output_fbo.end();
+        
+        m_output_fbo.draw(0, 0);
     }
 }
--- application/timely-matter/src/views/modules/particle_system/ParticleSystem.hpp
+++ application/timely-matter/src/views/modules/particle_system/ParticleSystem.hpp
@@ -12,11 +12,17 @@ namespace timelymatter {
         ofRectangle m_bounds;
         vector<Particle> m_particles;
         ofVboMesh m_particle_mesh;
+        ofMesh m_lines_mesh;
+        ofFbo m_particles_fbo;
+        ofFbo m_lines_fbo;
+        ofFbo m_output_fbo;
         ofParameterGroup m_params;
         ofParameter<bool> m_show_particles;
         ofParameter<float> m_max_velocity;
         ofParameter<float> m_velocity_decay;
         ofParameter<bool> m_show_mark_reference;
+        ofParameter<bool> m_draw_lines;
+        ofParameter<bool> m_draw_fading;
         
     public:
         void setup(const unsigned int num_particles, const ofVec3f size);
