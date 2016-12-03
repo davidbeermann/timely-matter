7684ab2b5e6382ac605b129dfc2cf48103c4d4fa
David Beermann <mail@davidbeermann.com>
2016-12-03 19:44:19 +0100

    added auto save functionality for kinect model after calibration

--- application/timely-matter/src/commands/UpdateProjectorCalibrationCommand.hpp
+++ application/timely-matter/src/commands/UpdateProjectorCalibrationCommand.hpp
@@ -10,10 +10,13 @@ namespace timelymatter {
     class UpdateProjectorCalibrationCommand : public Command {
     public:
         void execute(CalibrateProjectionArgs& args) {
+            bool auto_save = true;
+            
             if (args.selection_points.size() == 4) {
                 ofLog() << "Updated selection points.";
                 m_kinect_model.setSelectionPoints(args.selection_points);
             } else {
+                auto_save = false;
                 ofLog() << "invalid number of selection points: " << args.selection_points.size();
                 return;
             }
@@ -21,6 +24,7 @@ namespace timelymatter {
             if (args.depth_clip_near != -1.f) {
                 m_kinect_model.setDepthClipNear(args.depth_clip_near);
             } else {
+                auto_save = false;
                 ofLog() << "Value 'depth_clip_near' undefined: " << args.depth_clip_near;
                 return;
             }
@@ -28,8 +32,15 @@ namespace timelymatter {
             if (args.depth_clip_far != -1.f) {
                 m_kinect_model.setDepthClipFar(args.depth_clip_far);
             } else {
+                auto_save = false;
                 ofLog() << "Value 'depth_clip_far' undefined: " << args.depth_clip_near;
                 return;
+            }
+            
+            if (auto_save) {
+                m_kinect_model.saveSettings();
+            } else {
+                ofLogError() << "Autosave of Kinect Model failed";
             }
             
             // proceed to next state
